d1941a53ed2be4da0dcc08e9d450811ccb0d887e
Monteagudo <jpmonteagudo@liberty.edu>
2025-03-24 10:40:17 -0400

    first real commit

---
+++ R/simulate.R
@@ -0,0 +1,11 @@
+simulate_clt <- function(n = NULL,
+                         inverse = NULL,
+                         samples = 249){
+
+  stopifnot(is.null(n),
+            is.null(inverse),
+            is.null(samples) || is.numeric(samples)
+            )
+
+
+}
